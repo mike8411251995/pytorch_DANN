6fb8ee20b88a1de9820b1e7613330beaa8eded06
Mike Lee <mike8411251995@yahoo.com.tw>
2019-04-11 21:03:04 +0800

    modified params.py

--- train/params.py
+++ train/params.py
@@ -26,14 +26,26 @@ save_dir = './experiment'
 
 
 # specific dataset params
-extractor_dict = {'mnist_mnistm': models.Extractor(),
-                  'svhn_mnist': models.SVHN_Extractor(),
-                  'SynDig_svhn': models.SVHN_Extractor()}
+# extractor_dict = {'mnist_mnistm': models.Extractor(),
+#                   'svhn_mnist': models.SVHN_Extractor(),
+#                   'SynDig_svhn': models.SVHN_Extractor()}
 
-class_dict = {'mnist_mnistm': models.Class_classifier(),
-              'svhn_mnist': models.SVHN_Class_classifier(),
-              'SynDig_svhn': models.SVHN_Class_classifier()}
+# class_dict = {'mnist_mnistm': models.Class_classifier(),
+#               'svhn_mnist': models.SVHN_Class_classifier(),
+#               'SynDig_svhn': models.SVHN_Class_classifier()}
 
-domain_dict = {'mnist_mnistm': models.Domain_classifier(),
-               'svhn_mnist': models.SVHN_Domain_classifier(),
-               'SynDig_svhn': models.SVHN_Domain_classifier()}
+# domain_dict = {'mnist_mnistm': models.Domain_classifier(),
+#                'svhn_mnist': models.SVHN_Domain_classifier(),
+#                'SynDig_svhn': models.SVHN_Domain_classifier()}
+
+extractor_dict = {'usps_mnistm': models.Extractor(),
+                  'mnistm_svhn': models.Extractor(),
+                  'svhn_usps': models.Extractor()}
+
+class_dict = {'usps_mnistm': models.Class_classifier(),
+              'mnistm_svhn': models.Class_classifier(),
+              'svhn_usps': models.Class_classifier()}
+
+domain_dict = {'usps_mnistm': models.Domain_classifier(),
+               'mnistm_svhn': models.Domain_classifier(),
+               'svhn_usps': models.Domain_classifier()}
